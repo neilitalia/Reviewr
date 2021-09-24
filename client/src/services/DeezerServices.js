a584a0efe1007337ac0da4f9dac66fa0f22fa716
Neil Italia <neilitalia1998@gmail.com>
2021-09-24 10:28:20 -0500

    fix services

--- client/src/services/DeezerServices.js
+++ client/src/services/DeezerServices.js
@@ -2,27 +2,27 @@ import Axios from 'axios'
 import { DEEZER_BASE_URL } from '../globals'
 
 const Client = Axios.create({
-  baseURL: DEEZER_BASE_URL,
-  headers: {
-    'x-rapidapi-host': process.env.REACT_APP_API_HOST,
-    'x-rapidapi-key': process.env.REACT_APP_API_KEY
-  }
+  baseURL: DEEZER_BASE_URL
+  // headers: {
+  //   'x-rapidapi-host': process.env.REACT_APP_API_HOST,
+  //   'x-rapidapi-key': process.env.REACT_APP_API_KEY
+  // }
 })
 
-Client.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers['authorization'] = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => Promise.reject(error)
-)
+// Client.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token')
+//     if (token) {
+//       config.headers['authorization'] = `Bearer ${token}`
+//     }
+//     return config
+//   },
+//   (error) => Promise.reject(error)
+// )
 
 export const FindDeezerAlbum = async (data) => {
   try {
-    const res = await Client.get(`/search/album?q="${data}"`)
+    const res = await Client.get(`/search/album?q=${data}`)
     return res.data
   } catch (error) {
     throw error
@@ -40,7 +40,7 @@ export const GetDeezerAlbumDetails = async (data) => {
 
 export const FindDeezerArtist = async (data) => {
   try {
-    const res = await Client.get(`/search/artist?q="${data}"`)
+    const res = await Client.get(`/search/artist?q=${data}`)
     return res.data
   } catch (error) {
     throw error
